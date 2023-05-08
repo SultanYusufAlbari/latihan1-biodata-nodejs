5f848202b25e527b88f1641eb8b315d5797bf9ed
SultanYusufAlbari <129312544+SultanYusufAlbari@users.noreply.github.com>
2023-05-08 12:13:53 +0700

    satu kesatuan untuk run terminal about biodata

---
+++ latihan1-Biodata.js
@@ -0,0 +1,12 @@
+exports.nama = function (a) {
+    return a;
+};
+exports.tel = function (b) {
+    return b;
+};
+exports.tl = function (c) {
+    return c;
+};
+exports.alamat = function (d) {
+    return d;
+};
---
+++ latihan1-moduleBiodata.js
@@ -0,0 +1,14 @@
+var biodata = require('./latihan1-Biodata');
+
+var a= "Sultan Yusuf Albari";
+var b= "Bogor";
+var c= "09 September 2002";
+var d= "Kp. Babakan Indah No. 58";
+
+console.log("Nama : "+ biodata.nama(a));
+
+console.log("Tempat Lahir : "+ biodata.tel(b));
+
+console.log("Tanggal Lahir : "+ biodata.tl(c));
+
+console.log("Alamat : "+ biodata.alamat(d));
